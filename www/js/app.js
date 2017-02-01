e2a68bd3c96461da88502e60b9a4f0b2c235dfbe
EmperorTrajan <rojeantingal@gmail.com>
2017-02-01 09:29:56 +0800

    Final Modifications

--- www/js/app.js
+++ www/js/app.js
@@ -18,6 +18,7 @@ angular.module('starter', ['ionic'])
   });
 })
 
+
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('tabs', {
@@ -25,111 +26,6 @@ angular.module('starter', ['ionic'])
       abstract: true,
       templateUrl: 'templates/tabs.html'
     })
-
-    .state('tabs.home', {
-      url: '/home',
-      views: {
-        'home-tab' : {
-          templateUrl: 'templates/home.html'
-        }
-      }
-    })
-
-    .state('tabs.list', {
-      url: '/list',
-      views: {
-        'list-tab' : {
-          templateUrl: 'templates/list.html',
-          controller: 'ListController'
-        }
-      }
-    })
-
-    .state('tabs.detail', {
-      url: '/list/:aId',
-      views: {
-        'list-tab' : {
-          templateUrl: 'templates/detail.html',
-          controller: 'ListController'
-        }
-      }
-    })
-
-    .state('tabs.calendar', {
-      url: '/calendar',
-      views: {
-        'calendar-tab' : {
-          templateUrl: 'templates/calendar.html',
-          controller: 'CalendarController'
-        }
-      }
-    })
-
-    $urlRouterProvider.otherwise('/tab/home');
-})
-
-.controller('CalendarController', ['$scope', '$http', '$state',
-    function($scope, $http, $state) {
-    $http.get('js/data.json').success(function(data) {
-      $scope.calendar = data.calendar;
-
-      $scope.onItemDelete = function(dayIndex, item) {
-        $scope.calendar[dayIndex].schedule.splice($scope.calendar[dayIndex].schedule.indexOf(item), 1);
-      }
-
-      $scope.doRefresh =function() {
-      $http.get('js/data.json').success(function(data) {
-          $scope.calendar = data.calendar;
-          $scope.$broadcast('scroll.refreshComplete');
-        });
-      }
-
-      $scope.toggleStar = function(item) {
-        item.star = !item.star;
-      }
-
-    });
-}])
-
-.controller('ListController', ['$scope', '$http', '$state', 
-  function($scope, $http, $state) {
-    $http.get('js/data.json').success(function(data) {
-      $scope.artists = data.artists;
-      $scope.whichartist=$state.params.aId;
-      $scope.data={ showDelete: false; 
-        showReorder: false};
-
-      $scope.onItemDelete=function(item){
-        $scope.artists.splice($scope.artists.indexOf(item), 1);
-      }
-
-      $scope.doRefresh=function(){
-        $http.get('js/data.json').success(function(data)
-        {
-          $scope.artists = data.artists;
-          $scope.$broadcast('scroll.refreshComplete'
-            );
-        });
-      }
-
-      $scope.toggleStar=function(item){
-        item.star = !item.star;
-      }
-
-      $scope.moveItem = function(item, fromIndex, toIndex){
-        $scope.artists.splice(fromIndex, 1);
-        $scope.artists.splice(toIndex, 0, item);
-      };
-    });
-  }]); 
-/*
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-    .state('tabs', {
-      url: '/tab',
-      abstract: true,
-      templateUrl: 'templates/tabs.html'
-    }) 
 
     .state('tabs.home', {
       url: '/home',
@@ -224,5 +120,4 @@ angular.module('starter', ['ionic'])
         $scope.artists.splice(toIndex, 0, item);
       };
     });
-}]);*/
- 
+}]);
